f71de8ac3d22c9d45f4cf293df6c7f860123b9d8
Jess <jessrrobins@gmail.com>
2017-01-28 20:53:42 +0000

    more styling on the steps stuff

--- client/modules/App/components/About/About.js
+++ client/modules/App/components/About/About.js
@@ -1,7 +1,6 @@
 import React, { PropTypes, Component } from 'react';
 import { Media } from 'react-bootstrap';
-import { Card, CardDeck, CardImg, CardText, CardBlock,
-  CardTitle, CardSubtitle } from 'reactstrap';
+import { Col, Row } from 'reactstrap';
 import FontAwesome from 'react-fontawesome';
 
 if (process.env.BROWSER) {
@@ -10,25 +9,24 @@ if (process.env.BROWSER) {
 
 function About(props) {
   return (
-    <div className="container">
+    <div className='about'>
       <section>
-        <h2>What is everee?</h2>
+        <h1 className='about-title'>What is everee?</h1>
         <p>
           everee is a <strong>crowd-sourced bucket list for completionists</strong>.
         </p>
       </section>
+      
+      {StepsSection()}
+      
       <section>
-        <h2>How does it work?</h2>
-        {StepsSection()}
-      </section>
-      <section>
-        <h2>Explore</h2>
+        <h1 className='about-title'>Explore</h1>
         <p>
           recent lists
         </p>
       </section>
       <section>
-        <h2>Contact</h2>
+        <h1 className='about-title'>Contact</h1>
         <p>
           email, twitter
         </p>
@@ -39,33 +37,42 @@ function About(props) {
 
 function StepsSection(props) {
   return (
-    <CardDeck>
-      <Card block inverse color="primary">
-        <CardTitle className="vertical-align">
-          <FontAwesome name='plus' className='fa-4x fa-fw'/>
-          <span className="about-title">Add a new list</span>
-        </CardTitle>
-        <CardSubtitle>I want to... try ever flavor of oreo.</CardSubtitle>
-      </Card>
-      <Card block inverse color="primary">
-        <CardTitle className="vertical-align">
-          <FontAwesome name='search' className='fa-4x fa-fw'/>
-          <span className="about-title">everee searches lists</span>
-        </CardTitle>
-        <CardText>everee looks at existing lists to see if we have a collection of items that matches.
-        </CardText>
-      </Card>
-      <Card block inverse color="primary">
-        <CardTitle className="vertical-align">
-          <FontAwesome name='retweet' className='fa-4x fa-fw'/>
-          <span className="about-title">Your list stays up-to-date</span>
-        </CardTitle>
-        <CardText>When others add items to their list, the items
-          get added to yours too.
-        </CardText>
-        <CardSubtitle>A new flavor of oreo has been added to your list: Swedish Fish!</CardSubtitle>
-      </Card>
-    </CardDeck>
+    <section className="steps">
+      <div className='container'>
+        <h1 className='about-title'>How does it work?</h1>
+        <Row>
+          <Col md='4' xs='12' className='step'>
+            <FontAwesome name='plus' className='fa-4x fa-fw step-icon'/>
+            <div className="vertical-align">
+              <span className="steps-title">
+                <div className='step-number'>1</div>Add a new list
+              </span>
+            </div>
+          </Col>
+          <Col md='4' xs='12' className='step'>
+            <FontAwesome name='search' className='fa-4x fa-fw step-icon'/>
+            <div className="vertical-align">
+              <span className="steps-title">
+                <div className='step-number'>2</div>everee searches for your list
+              </span>
+            </div>
+            <p className='step-text'>everee looks at existing lists that others have made to see if we have a collection of items that matches.
+            </p>
+          </Col>
+          <Col md='4' xs='12' className='step'>
+            <FontAwesome name='retweet' className='fa-4x fa-fw step-icon'/>
+            <div className="vertical-align">
+              <span className="steps-title">
+                <div className='step-number'>3</div>Your list stays up-to-date
+              </span>
+            </div>
+            <p className='step-text'>When others add items to their list, the items
+              get added to yours too.
+            </p>
+          </Col>
+        </Row>
+      </div>
+    </section>
   )
 }
 
