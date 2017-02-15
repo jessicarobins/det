bda73bfeb7a68972abf33fba51a39689e3c6399f
Jess <jessrrobins@gmail.com>
2017-02-15 21:42:16 +0000

    getting filtering to work

--- client/modules/Post/components/PostList.js
+++ client/modules/Post/components/PostList.js
@@ -1,5 +1,5 @@
 import React, { Component, PropTypes } from 'react';
-import { Card, CardHeader, CardFooter, ListGroup, ListGroupItem, Badge } from 'reactstrap';
+import { Card, CardHeader, CardFooter, ListGroup, ListGroupItem, Badge, Input } from 'reactstrap';
 import FontAwesome from 'react-fontawesome';
 import { browserHistory } from 'react-router';
 import classnames from 'classnames';
@@ -17,7 +17,9 @@ class PostList extends Component {
     this.perPage = 8;
     
     this.state = {
-      startItemIndex: 0
+      startItemIndex: 0,
+      filter: '',
+      displayedLists: props.lists
     };
   }
   
@@ -31,7 +33,7 @@ class PostList extends Component {
   }
   
   showFooter() {
-    return this.lastItemIndex() < this.props.lists.length;
+    return this.lastItemIndex() < this.state.displayedLists.length;
   }
   
   showHeader() {
@@ -47,14 +49,40 @@ class PostList extends Component {
     this.setState({startItemIndex: newStartIndex});
   }
   
+  filter = () => {
+    this.setState({filter: this.searchRef.value});
+    const lists =  _.filter(this.props.lists, o => { 
+      if (!this.state.filter.length) {
+        return true;
+      }
+      return o.name.includes(this.state.filter);
+    });
+    this.setState({
+      startItemIndex: 0,
+      displayedLists: lists
+    });
+  }
+  
+  full() {
+    return !this.showHeader() && this.state.displayedLists.length > this.perPage;
+  }
+  
   render() {
     return (
       <Card 
         className={classnames({ 
-          full: !this.showHeader(),
+          full: this.full(),
           'list-list': true
         })}>
-        <CardHeader tag="h3" className='you-wanna'>You want to...</CardHeader>
+        <CardHeader tag="h3" className='you-wanna'>
+          You want to... 
+          <Input
+            getRef={ref => {this.searchRef = ref}} 
+            onChange={this.filter}
+            name="search" 
+            id="search" 
+            placeholder="placeholder" />
+        </CardHeader>
         { 
           this.showHeader() &&
           <CardHeader 
@@ -65,7 +93,8 @@ class PostList extends Component {
         }
         <ListGroup className='list-group-flush'>
           {
-            _.slice(this.props.lists, this.state.startItemIndex, this.lastItemIndex())
+            _.chain(this.state.displayedLists)
+              .slice(this.state.startItemIndex, this.lastItemIndex())
               .map(list => (
                 <ListGroupItem
                   className="justify-content-between"
@@ -80,6 +109,7 @@ class PostList extends Component {
                   
                 </ListGroupItem>
               ))
+              .value()
           }
         </ListGroup>
         {
