95b0e11041ebc2c9d6fe44324e63041314302b33
Jess <jessrrobins@gmail.com>
2017-02-20 18:22:44 +0000

    make uniqueness sparse

--- server/models/user.js
+++ server/models/user.js
@@ -5,7 +5,7 @@ const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
   email: { type: String, unique: true, lowercase: true },
-  username: { type: String, unique: true, uniqueCaseInsensitive: true },
+  username: { type: String, unique: true, sparse: true, uniqueCaseInsensitive: true },
   name: { type: String, default: '' },
   picture: { type: String, default: '' },
   oauthID: { type: Number },
